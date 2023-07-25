f8911e7b07a8c48cc80e1ef402d804f6996af9c7
Filipp Frizzy <filipp.s.frizzy@gmail.com>
2023-07-25 23:56:27 +0400

    up codesandbox setup script

--- .codesandbox/setup.sh
+++ .codesandbox/setup.sh
@@ -1,8 +1,10 @@
-docker login --username adaptframework --password="$DOCKER_PASSWORD"
+[[ ! -z "$DOCKER_PASSWORD" ]] && docker login --username adaptframework --password="$DOCKER_PASSWORD"
 
-id=$(docker create adaptframework/mufl:sha-4a396e3)
-docker cp $id:/mufl /mufl
-docker rm -v $id
+if [ ! -d "/mufl" ]; then
+    id=$(docker create adaptframework/mufl:sha-4a396e3)
+    docker cp $id:/mufl /mufl
+    docker rm -v $id
+fi
 
 cd /mufl && sh build-nodejs-modules.sh
 
