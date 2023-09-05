b98950d201e08510fde500fc61453310bcec3573
Vitalii Shakhmatov <vitalii@adaptframework.solutions>
2023-09-05 11:48:49 +0200

    remove broker settings, pass broker address as a docker build arg

--- web/scripts/init_env.sh
+++ web/scripts/init_env.sh
@@ -3,3 +3,5 @@
 REACT_APP_MUFLO_CODE_HASH=$(ls -t ../mufl_code/*.muflo | head -n 1 | xargs basename | sed 's/\.muflo$//')
 
 echo "REACT_APP_MUFLO_CODE_HASH = \"$REACT_APP_MUFLO_CODE_HASH\"" > .env
+
+echo "REACT_APP_BROKER_ADDRESS = \"$__BROKER_ADDRESS__\"" >> .env
--- web/src/DemoIntroPage.tsx
+++ web/src/DemoIntroPage.tsx
@@ -1,10 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import './DemoIntroPage.css';
 
-const DemoIntroPage: React.FC = () => {
-    const [brokerAddress, setBrokerAddress] = useState<string>(`ws://${window.location.hostname}:9001`);
-    const [showSettings, setShowSettings] = useState<boolean>(false);
-    
+const DemoIntroPage: React.FC = () => {    
     const generateSeed = () => {
         return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
     }
@@ -13,22 +10,6 @@ const DemoIntroPage: React.FC = () => {
         const seed = generateSeed();
         window.open(`/MainPage?seed=${seed}`, "_blank");
     }
-
-    const handleBrokerAddressChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setBrokerAddress(event.target.value);
-    }
-
-    const handleSubmitBrokerAddress = () => {
-        localStorage.setItem("brokerAddress", brokerAddress);
-        setShowSettings(false);
-    }
-
-    useEffect(() => {
-        const savedBrokerAddress = localStorage.getItem("brokerAddress");
-        if (savedBrokerAddress) {
-            setBrokerAddress(savedBrokerAddress);
-        }
-    }, []);
 
 
     return (
@@ -41,16 +22,6 @@ const DemoIntroPage: React.FC = () => {
                 </p>
                 <h3>Give it a try</h3>
                 <button onClick={handleButtonClick}>Launch messenger</button>
-                <div className="settings-section">
-                    <button onClick={() => setShowSettings(!showSettings)}>Settings</button>
-                    {showSettings && (
-                        <div className="broker-settings">
-                            <label>Broker Address: </label>
-                            <input type="text" value={brokerAddress} onChange={handleBrokerAddressChange} />
-                            <button onClick={handleSubmitBrokerAddress}>Update</button>
-                        </div>
-                    )}
-                </div>
             </div>
         </div>
     );
